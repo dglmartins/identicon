e269c954ee77dafae4e98b7be14826b208fce32c
danilomartins <dglmartins@outlook.com>
2018-05-13 22:45:55 -0400

    build grid and mirror row

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,6 +2,28 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  # the argument received here is image, however, we pattern match right inside the argument list.
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail ]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
@@ -11,6 +33,4 @@ defmodule Identicon do
 
     %Identicon.Image{hex: hex}
   end
-
-
 end
